573cccbf9e855e8cb80f8f78f23a3f72f72fed3f
Radies <ranyoung0102@gmail.com>
2025-04-05 20:53:50 +0900

    fast push directory settings

---
+++ src/test/java/com/library/devpulse/DevPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.devpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
